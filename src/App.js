a2a1d10cac5008ab504e15dce0ace998bfa35516
angela <angelagargallo@libero.it>
2019-07-18 15:11:28 +0200

    react conditional render pratic

--- src/App.js
+++ src/App.js
@@ -13,9 +13,12 @@ constructor() {
   this.state = {
     todos: todosData,
     isLoading: true, 
-    unreadMessages: ["a","b"]
+    unreadMessages: ["a","b"], 
+
+    isLoggedIn: false
   }
   this.handleChange = this.handleChange.bind(this)
+  this.handleClick = this.handleClick.bind(this)
 }
 
     
@@ -41,25 +44,40 @@ handleChange(id) {
   })
 }
 
+handleClick(){
+this.setState(prevState => {
+  return {
+    isLoggedIn: !prevState.isLoggedIn
+  }
+})
+} 
+
   render() {
     const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
     handleChange={this.handleChange}/>)
+
+    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+    let displayText = this.state.isLoggedIn ? "LOGGED OUT" : "LOGGED IN"
     return (
       <div className="todo-list">
+
         {todoItems}
+
         <div>
           {this.state.isLoading ?
           <h1>Loading...</h1> :
           <Conditional />}
         </div>
+
         <div>
-                {
-                    this.state.unreadMessages.length > 0 && 
-                    <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-                }
-            </div>
+          {this.state.unreadMessages.length > 0 && 
+          <h2>You have {this.state.unreadMessages.length} unread messages!</h2>}
+        </div>
 
-
+        <div>
+          <button onClick={this.handleClick}>{buttonText}</button>
+          <h1>{displayText}</h1>
+        </div>
       </div>
    
     );
