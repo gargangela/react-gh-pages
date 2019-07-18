f60b43c5e65323267682a4fa981c0df12c8d0bf7
angela <angelagargallo@libero.it>
2019-07-18 10:26:01 +0200

    react todo app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TodoItem from "./TodoItem";
+import todosData from "./todosData";
+
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+constructor() {
+  super()
+  this.state = {
+    todos: todosData
+  }
+  this.handleChange = this.handleChange.bind(this)
+}
+
+
+handleChange(id) {
+  this.setState(prevState => {
+    const updatedTodos = prevState.todos.map (todo =>{
+      if (todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    return {
+      todos: updatedTodos
+    }
+  })
+}
+
   render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+    handleChange={this.handleChange}/>)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="todo-list">
+        {todoItems}
       </div>
     );
   }
---
+++ src/todosData.js
@@ -0,0 +1,30 @@
+const todosData = [
+    {
+        id: 1,
+        text:"text 1 ",
+        completed:true
+    },
+    {
+        id: 2,
+        text:"text 2",
+        completed: false
+    },
+    {
+        id: 3,
+        text:"text 3 ",
+        completed: false
+    },
+    {
+        id: 4,
+        text:"text 4 ",
+        completed: false
+    },
+    {
+        id: 5,
+        text:"text 5 ",
+        completed: false
+    }
+
+]
+
+export default todosData
